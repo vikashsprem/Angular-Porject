cebf64aaffef0291ffbfc41b3973c19908cec57e
Vikash Prem Sharma <vikashkumarukg977@gmail.com>
2024-03-02 14:29:11 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ThemeToggleComponent } from './theme-toggle/theme-toggle.component';
+import { MatIconModule } from '@angular/material/icon'; // Import MatIconModule
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ThemeToggleComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/theme-toggle/theme-toggle.component.ts
@@ -0,0 +1,21 @@
+
+import { Component } from '@angular/core';
+import { ThemeService } from '../theme.service';
+
+@Component({
+  selector: 'app-theme-toggle',
+  templateUrl: './theme-toggle.component.html',
+  styleUrls: ['./theme-toggle.component.css'],
+})
+export class ThemeToggleComponent {
+  isDarkMode: boolean;
+
+  constructor(private themeService: ThemeService) {
+    this.isDarkMode = this.themeService.isDarkMode();
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    this.themeService.setDarkMode(this.isDarkMode);
+  }
+}
